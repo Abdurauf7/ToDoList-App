1407b2ac4675b6cebd8ed95b52cf7f70d14cbf78
Abdurauf <48782337+Abdurauf7@users.noreply.github.com>
2020-10-13 08:43:29 +0500

    TodoList

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, ipsum impedit ad nihil saepe perferendis. Exercitationem natus earum iure deleniti, cum reprehenderit quas praesentium, sunt, officiis suscipit in nam? Voluptate.</p>
+    <router-link to="/todos">Todos</router-link>
+</div>
+</template>
